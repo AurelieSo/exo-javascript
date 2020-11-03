b1e0a2a248caed76f8303de84241e1f55757948c
AurelieSo <soucek.aurelie@mgail.com>
2020-11-03 11:36:38 +0100

    ajout exo 4 et 5

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+
+
+function sendData() {
+    var firstNumber= document.getElementById('firstNumber').value;
+    var secondNumber= document.getElementById('secondNumber').value;
+    alert ('le reste est : '+ firstNumber % secondNumber);
+    }
